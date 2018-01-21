c9c3260dd86c803cf4a1e4719d6e98aaa615198a
kavita <kavita.bora@tothenew.com>
2018-01-21 21:29:00 +0530

    spring security basic done.

---
+++ spring-framework-mvc-java-config/src/main/java/com/springmvc/repositories/VerificationTokenRepository.java
@@ -0,0 +1,10 @@
+package com.springmvc.repositories;
+
+import com.springmvc.entity.User;
+import com.springmvc.entity.VerificationToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VerificationTokenRepository extends CrudRepository<VerificationToken,Integer> {
+
+    VerificationToken findByToken(String token);
+}
