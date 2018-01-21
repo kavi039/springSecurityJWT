0c80703aa36d165c3e23a196adba08109efbfd07
kavita <kavita.bora@tothenew.com>
2018-01-21 21:36:52 +0530

    rename project.

--- spring-framework-mvc-java-config/src/main/java/com/springmvc/config/SpringSecurityFilterConfig.java
+++
@@ -1,6 +0,0 @@
-package com.springmvc.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityFilterConfig extends AbstractSecurityWebApplicationInitializer {
-}
